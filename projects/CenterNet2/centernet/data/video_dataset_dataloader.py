c164f83b664f45774b1455c00141f27ecbee597a
JEFFREY ZHANG <jozhang@berkeley.edu>
2021-07-12 12:45:09 -0500

    move get_video_dataset_dicts

--- projects/CenterNet2/centernet/data/video_dataset_dataloader.py
+++ projects/CenterNet2/centernet/data/video_dataset_dataloader.py
@@ -140,40 +140,6 @@ def split_video_dataset(dataset, test_len, stride):
     return ret
 
 
-def get_video_dataset_dicts(
-    dataset_names, filter_empty=True, gen_inst_id=False,
-):
-    assert len(dataset_names)
-    dataset_dicts = [DatasetCatalog.get(dataset_name) for dataset_name in dataset_names]
-    for dataset_name, dicts in zip(dataset_names, dataset_dicts):
-        assert len(dicts), "Dataset '{}' is empty!".format(dataset_name)
-
-    inst_count = 1000000
-    video_datasets = []
-    for source_id, (dataset_name, dicts) in enumerate(
-        zip(dataset_names, dataset_dicts)):
-        assert len(dicts), "Dataset '{}' is empty!".format(dataset_name)
-        videos = {}
-        single_video_id = 1000000
-        for image in dicts:
-            video_id = image.get('video_id', -1)
-            if video_id == -1:
-                single_video_id = single_video_id + 1
-                video_id = single_video_id
-            if video_id not in videos:
-                videos[video_id] = {
-                    'video_id': video_id, 'images': [],
-                    'dataset_source': source_id}
-            if gen_inst_id:
-                for x in image['annotations']:
-                    if 'instance_id' not in x:
-                        inst_count += 1
-                        x['instance_id'] = inst_count
-            videos[video_id]['images'].append(image)
-        video_datasets.append([v for v in videos.values()])
-    video_datasets = list(itertools.chain.from_iterable(video_datasets))
-    return video_datasets
-
 
 class SingleGPUInferenceSampler(Sampler):
     def __init__(self, size: int):
